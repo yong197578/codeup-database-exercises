a67d56b8169b791567544e42483400fcd5ef4c2d
Yong Choi <yong.s.choi2@gmail.com>
2023-05-10 14:47:52 -0500

    where-exercises update

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,25 @@
 use employees;
 
+#     select * from employees
+#     where first_name IN ('Irena', 'Vidya', 'Maya');
+#     select * from employees
+#     where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
     select * from employees
-    where first_name = ('Irena', 'Vidya', )
+    where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
+    and  gender ='M';
+
+    select * from employees
+    where last_name like 'E%';
+
+    select distinct * from employees
+    where last_name like '%E' or last_name like 'E%';
+
+    select distinct * from employees
+    where last_name like '%E' and last_name like 'E%';
+
+    select * from employees
+    where last_name like '%q%';
+
+    select * from employees
+    where last_name like '%q%' and last_name not like '%qu%';
